c8d6a2b94ab54d784dcdc9af63eecef2bccacde5
Joanna Lew/javascript-blog.git <lewjoanna96@gmail.com>
2023-04-13 11:27:17 +0200

    change title

--- src/components/PagesNotFound/PagesNotFound.js
+++ src/components/PagesNotFound/PagesNotFound.js
@@ -4,10 +4,7 @@ import styles from './PagesNotFound.module.scss';
 const PagesNotFound = () => {
   return (
     <div className={styles.PagesNotFound}>
-      <h1>We lost this page</h1>
-      <p>We searched high and low but couldn't find what you're looking for.</p>
-      <p>Let's find better place for you to go</p>
-      <Button><a href="/">Home</a></Button>
+      <h1>404 NOT FOUND</h1>
     </div>
   );
 };
